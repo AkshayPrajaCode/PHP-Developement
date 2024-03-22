e2762b03ddca3510f3fdde1067884e3d3642bafa
AkshayPrajaCode <akshaykumarprajapati@hotmail.com>
2024-03-22 20:32:07 +0530

    get

---
+++ learn/fileone.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>From Page</title>
+
+</head>
+
+<body>
+  <form action="Action.php" method="get">
+  Name : <input type="text" name="name" id="name"> <br><br>
+  Age : <input type="number" name="age" id="age"><br><br>
+  <input type="submit" value="submit">
+  </form>
+</body>
+
+</html>
